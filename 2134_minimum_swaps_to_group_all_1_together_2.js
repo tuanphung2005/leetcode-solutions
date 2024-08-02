7e7617baf195b1c7c04d3776464c251bfb1b2c9c
PHUNG ANH TUAN <76047829+tuanphung2005@users.noreply.github.com>
2024-08-02 22:13:57 +0700

    Add files via upload

--- 2134_minimum_swaps_to_group_all_1_together_2.js
+++ 2134_minimum_swaps_to_group_all_1_together_2.js
@@ -1,20 +1,25 @@
-function minimumSwapsToGroupAll1Together(nums) {
+function minSwaps(nums) {
     let ones = 0;
     for (let i = 0; i < nums.length; i++) {
         if (nums[i] === 1) ones++;
     }
-    
+
+    // Extend the array to handle the circular nature
+    let extendedNums = nums.concat(nums);
+
     let window = 0;
     for (let i = 0; i < ones; i++) {
-        if (nums[i] === 0) window++;
+        if (extendedNums[i] === 0) window++;
     }
-    
+
     let minSwaps = window;
-    for (let i = ones; i < nums.length; i++) {
-        if (nums[i] === 0) window++;
-        if (nums[i - ones] === 0) window--;
+    for (let i = ones; i < nums.length + ones; i++) {
+        if (extendedNums[i] === 0) window++;
+        if (extendedNums[i - ones] === 0) window--;
         minSwaps = Math.min(minSwaps, window);
     }
-    
+
     return minSwaps;
 }
+
+console.log(minSwaps([1, 1, 0, 0, 1])); // Output should be 0
